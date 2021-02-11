a54fda32ea964b6c4973e6c6fb68959c9c3009e5
makian <makian.gpc>
2021-02-11 21:07:58 +0900

    [Android] Activity と Fragment の画面遷移の実装

--- FlagmentTest_210211/app/src/main/java/com/example/flagmenttest_210211/MainActivity.java
+++ FlagmentTest_210211/app/src/main/java/com/example/flagmenttest_210211/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,18 +15,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button01 = findViewById(R.id.button);
+
         if(savedInstanceState == null){
-            // FragmentManagerのインスタンス生成
-            FragmentManager fragmentManager = getSupportFragmentManager();
 
-            // FragmentTransactionのインスタンスを取得
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            button01.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // FragmentManagerのインスタンス生成
+                    FragmentManager fragmentManager = getSupportFragmentManager();
 
-            // インスタンスに対して張り付け方を指定する
-            fragmentTransaction.replace(R.id.container, new SampleFragment());
+                    // FragmentTransactionのインスタンスを取得
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-            // 張り付けを実行
-            fragmentTransaction.commit();
+                    // BackStackを設定
+                    fragmentTransaction.addToBackStack(null);
+
+                    // インスタンスに対して張り付け方を指定する
+                    fragmentTransaction.replace(R.id.container, new SampleFragment());
+
+                    // 張り付けを実行
+                    fragmentTransaction.commit();
+                }
+            });
         }
     }
 }
--- FlagmentTest_210211/app/src/main/java/com/example/flagmenttest_210211/SampleFragment.java
+++ FlagmentTest_210211/app/src/main/java/com/example/flagmenttest_210211/SampleFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,10 +12,33 @@ import androidx.fragment.app.Fragment;
 
 public class SampleFragment extends Fragment {
 
+    public static SampleFragment newInstance(String str){
+        // Fragment01 インスタンス生成
+        SampleFragment fragment = new SampleFragment ();
+        // Bundle にパラメータを設定
+        Bundle barg = new Bundle();
+        barg.putString("Message", str);
+        fragment.setArguments(barg);
+
+        return fragment;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main,
                 container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Bundle args = getArguments();
+        if(args != null ){
+            String str = args.getString("Message");
+            TextView textView = view.findViewById(R.id.text_fragment);
+            textView.setText(str);
+        }
     }
 }
